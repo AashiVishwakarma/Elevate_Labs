2ae474afe9f79ec6678eb1e195a52c2f4ea7c46a
demo <aashivishwakarma1121@gmail.com>
2025-08-04 15:29:11 +0530

    second commit

--- Task1.java
+++ Task1.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Task1 {
-    public static int addition(int num1,int num2){
+    public static double addition(double num1,double num2){
         return num1+num2;
     }
-    public static int subtract(int num1,int num2){
+    public static double subtract(double num1,double num2){
         return num1-num2;
     }
-    public static int multiply(int num1,int num2){
+    public static double multiply(double num1,double num2){
         return num1*num2;
     }
-    public static int divide(int num1,int num2){
+    public static double divide(double num1,double num2){
         if(num2==0){
             System.out.println("Error: Cannot divide by zero!");
         }
@@ -21,11 +21,11 @@ public class Task1 {
         boolean calculator=true;
         while(calculator){
             System.out.print("Enter the first number : ");
-            int num1=sc.nextInt();
+            double num1=sc.nextDouble();
             System.out.print("Enter the operator (+ , - , * , /) : ");
             char op=sc.next().charAt(0);
             System.out.print("Enter the second number : ");
-            int num2=sc.nextInt();
+            double num2=sc.nextDouble();
             double result;
             switch (op){
                 case '+':
@@ -45,9 +45,9 @@ public class Task1 {
                     continue;
             }
             System.out.println(result);
-            System.out.print("Do you want to perform another calculation? (yes/no): ");
+            System.out.print("Do you want to calculate again? Type 'yes' or 'y' to continue: ");
             String answer = sc.next();
-            if (!answer.equalsIgnoreCase("yes")) {
+            if (!(answer.equals("yes") || answer.equals("y"))) {
                 calculator = false;
                 System.out.println("Calculator closed.");
             }
